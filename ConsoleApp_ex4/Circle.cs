441614c28f3d797cd942c977feff8eb92055d0c3
Admin <Admin@DESKTOP-IOMF4D2>
2022-11-11 00:26:05 +0100

    done

---
+++ ConsoleApp_ex4/Circle.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp_ex3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ex4
+{
+    internal class Circle
+    {
+        Point centre = new();
+        double radius;
+        Circle(Point point, double r )
+        {
+            this.centre = point;
+            this.radius = r;
+        }
+        public double r
+        {
+            get
+            { return radius; }
+
+            set
+            {
+                radius = value;
+            }
+        }
+        public Point p
+        {
+            get
+            { return centre; }
+
+            set
+            {
+                centre = value;
+            }
+        }
+
+        public bool Egalite(Circle d)
+        {
+            if ((d.radius == this.radius) && (d.centre == this.centre))
+                return true;
+            return false;
+        }
+        public bool intersection(Circle c)
+        {
+            if (centre.Distance(c.centre) < (c.radius+this.radius))
+                return true;
+            return false;
+        }
+        public double Circonférence()
+        {
+            return 3.14 * this.radius * 2;
+        }
+        public double air()
+        {
+            return 3.14 * this.radius * this.radius;
+        }
+    }
+}
---
+++ ConsoleApp_ex4/Droit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp_ex3;
+
+namespace ConsoleApp_ex4
+{
+    internal class Droit : Point
+    {
+
+
+        double coefficiant;
+        double rest;
+
+        Droit(double a ,double b)
+        {
+            this.coefficiant = a;
+            this.rest = b;
+        }
+        public double A {
+            get 
+            { return coefficiant; }
+
+            set {
+                coefficiant = value;
+            } }
+        public double B
+        {
+            get
+            { return rest; }
+
+            set
+            {
+                rest = value;
+            }
+        }
+
+
+
+
+        public bool Egalite(Droit d)
+        {
+            if ((d.coefficiant == this.coefficiant) && (d.rest == this.rest))
+                return true;
+            return false;
+        }
+        public bool parallelisme(Droit d)
+        {
+            if ((d.coefficiant == this.coefficiant))
+                return true;
+            return false;
+        }
+        public Point? intersection(Droit d)
+        {
+            if (this.parallelisme(d))
+                return null;
+
+            
+            double x, y;
+
+            x = (d.rest - this.rest)/(this.coefficiant -  d.coefficiant);
+            y = this.coefficiant * x + this.rest;
+
+            Point point = new Point(x,y);
+
+            return point;
+        }
+        public bool perpendicularité(Droit d)
+        {
+            if (this.coefficiant * d.coefficiant == -1)
+            return true;
+
+            return false;
+        }
+    }
+}
